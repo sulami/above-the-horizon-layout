c9b8c7806cbe490017e629e3fcd4e4992e870d78
Robin Schroer <sulami@peerwire.org>
2018-08-25 18:44:27 +0100

    Add a task class

---
+++ Above The Horizon/Task.swift
@@ -0,0 +1,17 @@
+//
+//  Task.swift
+//  Above The Horizon
+//
+//  Created by Robin Schroer on 2018-08-25.
+//  Copyright © 2018 Robin Schroer. All rights reserved.
+//
+
+import Foundation
+
+class Task {
+    @objc dynamic var title: String
+
+    init(title: String) {
+        self.title = title
+    }
+}
