08f4c4f4ce3832eef1bbf87baf988ea3eeda5e80
Robin Schroer <sulami@peerwire.org>
2018-08-25 13:17:06 +0100

    Initial Commit

---
+++ Above The Horizon/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Above The Horizon
+//
+//  Created by Robin Schroer on 2018-08-25.
+//  Copyright © 2018 Robin Schroer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
